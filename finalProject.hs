de996d2c2363b7310eece2b4cda481cde44305a4
Peter Gessler <pgessler2@gmail.com>
2022-12-01 11:41:45 -0600

    Added eval functionality

--- finalProject.hs
+++ finalProject.hs
@@ -9,6 +9,7 @@ data TYPELANG where
   deriving (Show,Eq)
 data VALUELANG where
   NumV :: Int -> VALUELANG
+  BoolV :: Bool -> VALUELANG
   ClosureV :: String -> TERMLANG -> ValueEnv -> VALUELANG
   deriving (Show,Eq)
 
@@ -72,3 +73,45 @@ typeofM g (Lambda i d b) = do {r <- typeofM ((i,d):g) b;
 typeofM g (App f a) = do { a' <- typeofM g a;
                           (d:->:r) <- typeofM g f;
                           if a'==d then return r else Nothing}
+
+
+evalM :: ValueEnv -> TERMLANG -> (Maybe VALUELANG)
+evalM e (Num x) = Just (NumV x)
+evalM e (Id id) = lookup id e
+evalM e (Plus l r) = do{ (NumV l') <- (evalM e l);
+                         (NumV r') <- (evalM e r);
+                         return (NumV (l' + r'))}
+evalM e (Minus l r) = do{ (NumV l') <- (evalM e l);
+                         (NumV r') <- (evalM e r);
+                         return (NumV (l' - r'))}
+evalM e (Mult l r) = do{ (NumV l') <- (evalM e l);
+                         (NumV r') <- (evalM e r);
+                         return (NumV (l' * r'))}
+evalM e (Div l r) = do{ (NumV l') <- (evalM e l);
+                         (NumV r') <- (evalM e r);
+                         if r'==0 then Nothing
+                         else return (NumV (l' `div` r'))}
+evalM e (Boolean b) = Just (BoolV b)
+evalM e (And l r) = do{ (BoolV l') <- (evalM e l);
+                        (BoolV r') <- (evalM e r);
+                        return (BoolV (l' && r'))}
+evalM e (Or l r) = do{ (BoolV l') <- (evalM e l);
+                        (BoolV r') <- (evalM e r);
+                        return (BoolV (l' || r'))}
+evalM e (Leq l r) = do{ (NumV l') <- (evalM e l);
+                        (NumV r') <- (evalM e r);
+                        return (BoolV (l' <= r'))}
+evalM e (IsZero v) = do{ (NumV v') <- (evalM e v);
+                          return (BoolV (v'==0))}
+evalM e (If c t e') = do{ (BoolV c') <- (evalM e c);
+                         (if c' then (evalM e t) else (evalM e e'))}
+evalM e (Lambda i d b) = Just (ClosureV i b e)
+evalM e (App f a) = do{ (ClosureV i b e) <- (evalM e f);
+                        (a') <- (evalM e a);
+                        (evalM ((i,a'):e) b)}
+
+main :: IO()
+main = do
+        print (typeofM[("n",(TNum))](App(Lambda "x" TNum (Plus(Id "x")(Id "n")))(Num 4)))
+        print (evalM [("n",(NumV 3))] (App(Lambda "x" TNum (Plus(Id "x")(Id "n")))(Num 4)))
+        print (evalM [("x",(NumV 4)),("y", NumV 4),("b", (BoolV False))] (If (Id "b") (Mult(Id "x")(Id "y")) (Minus(Id "x")(Id "y"))))
